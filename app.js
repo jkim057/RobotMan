c954a2b6c67c197faa8987dbaf900b4c5259602c
jkim057 <joshkim057@gmail.com>
2021-04-29 14:49:09 -0400

    CSS Styling V1

---
+++ app.js
@@ -0,0 +1,6 @@
+// break up string into arrays, create list items & div spots per string item // border on bottom of div to make the letters presentable.
+
+// for each string of 5 or less letters, a certain amount of spacing and space 
+// every 5 more letters will increase the space taken and REDUCE the borderspacing before and after the word.
+// words to guess can be invisible, and and once guessed correctly will turn visible.
+
