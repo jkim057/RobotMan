a9ed1183e6169065c2565d9f8386112bf42af7d5
jkim057 <joshkim057@gmail.com>
2021-04-30 09:58:56 -0400

    JS setup str split

--- app.js
+++ app.js
@@ -4,3 +4,42 @@
 // every 5 more letters will increase the space taken and REDUCE the borderspacing before and after the word.
 // words to guess can be invisible, and and once guessed correctly will turn visible.
 
+const subBtn = document.querySelector("#bars")
+const inputBar = document.querySelector("#text")
+
+
+function stringBreak(str){
+    return str.split("") 
+}
+
+subBtn.addEventListener("click", event => {
+    event.preventDefault()
+
+
+    for (let i = 0; i < str.length; i++) {
+        if (inputBar.value == [i]) {
+            return 
+        
+    }
+
+
+    if (inputBar.value) {
+        
+    }
+
+
+    console.log(inputBar.value)
+    // check if the input value exists on the preset guess words
+        // save words as a string in an array
+                // word strings need to be broken up into strings of individual letters 
+                // function to break up strings individually needs to be reuseable if we are going to let players enter in words to guess.
+    // create li item & use innerText to put in letter value
+    const li = document.createElement("li")
+
+
+
+
+    // .appendChild()
+})
+
+console.log(stringBreak("word"))
