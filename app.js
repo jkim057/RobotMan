643618797e43f05d3ebc5b43ab32a29f8902a23d
jkim057 <joshkim057@gmail.com>
2021-05-02 18:52:49 -0400

    HTML CS JS Cleaned up code

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 // Notes:
 
-// Letter LI setup goals/plans:
+// Letter setup goals/plans:
     // 1. break up string into arrays ✅
     // 2. create list items dynamically ✅
     // 3. div spots per string item ✅
-    // border on bottom of div to make the letters presentable (CSS) ✅
-        // A. for each string of 5 or less letters, a certain amount of spacing has to be setup in front and behind of the letters.
-        // Status: this has been roughly done :check:
-// every additional 5 letters (tiers):
-    // 1. increase the total space taken up &
-    // 2. REDUCE the borderspacing before and after the word.
-// words to guess can be invisible, and and once guessed correctly will turn visible. (Maybe?)
+    // 4. border on div bottom to make placeholder (CSS) ✅
+        // OPTIONAL: 
+        // A. the front and back of strings need spacing (center?)
+        // B. set up Tiers for every 5 letters
+            // 1. increase the total space taken up &
+            // 2. REDUCE borderspacing before and after letters.
+    // letters to guess should be invisible, & when guessed correctly will turn visible. ✅ ABSOLUTELY CHECK
 
-/// Li.innerText lines have beem moved //////
-  // check if the input value exists on the preset guess words ✅
-        // save words as a string in an array
-        // word strings need to be broken up into strings of individual letters ✅
-    // function to break up strings individually needs to be reuseable if we are going to let players enter in words to guess. ✅
-    // create li item & use innerText to put in letter value ✅
+    // 5. check if the input value exists on the preset guess words ✅
+        // A. save words as a string in an array ✅
+        // B. need to break up word string into string letters ✅
+    // OPTIONAL: function to break up strings need to be reuseable ✅
+    // 6. create letter item & use innerText to put in letter value ✅
 ///////////////////////////
 
 // if true, createElement li w letter input under images ✅
-// if false, have an element listener that will have one of the images show up
+// if false, make one of the images show up instead
+
+// functions:
+    // Note: look ma i figured out how to write a anonymous function that doesn't break or make me cry ✅
+    // () => Math.floor(Math.random() * wordsWordsWords.length);
+
+    // 1. generate random # limited to the length of the guess word ✅
+    // 2. breaks up string into array of strings ✅
+    // 3. combo str break & i break into 1 variable function ✅
+        // OPTIONAL: need to setup event listener for this function on a opening page for when players want to enter in words 
+
+
+
+
 
 ////////// Variables ////////// 
 
@@ -33,12 +45,7 @@ const chosenWord = ["panda","focus","hocus","prime","limbs","tears","clubs","wee
 const lettersShow = document.querySelector('.display')
 
 //////////  Functions ////////// 
-// look ma i figured out how to write a anonymous function that doens't break or make me cry ✅
-//  () => Math.floor(Math.random() * wordsWordsWords.length);
 
-// 1. generates random number limited to the length of the guess word
-// 2. breaks up string into array of strings
-// 3. combo str break & i break into 1 variable function 
 const indexRandom = () => {return Math.floor(Math.random() * chosenWord.length)}
 const stringBreak = (str) => {return str.split("") }
 
@@ -54,51 +61,28 @@ const setUpWords = () => {
         lettersSpan.innerText = wordArray[i]
         lettersShow.appendChild(letterStyle)
         letterStyle.appendChild(lettersSpan)  
-
     }
 }
+// need to get setUpWords to run automatically 
 setUpWords()
-    
-
-
-
-
 
 const letterFind = function(){
     for (let i = 0; i < wordArray.length; i++) {
         if (wordArray[i].includes(inputBar.value)){
             let reveal = document.querySelector(`#letterPosition${i}`)
             reveal.style.visibility = "visible";
-    }
-
-    }
-}
-
-
-// to every item in the array that means the requirements, 
-// add toggle class
-
-// if doesn't match, do nothing.
+}}}
 
 //////////  Event Listener ////////// 
 
 subBtn.addEventListener("click", (event) => {
     event.preventDefault()
         if(wordArray.includes(inputBar.value)){
-            console.log(wordArray)
-            // step 1. need to find the specific wordArray item 
-            // step 2. need to add this toggle class to that item
-            // step 3. hope to god that this toggle class will make this show up
             letterFind()
-
-
-            // return
-    
         }else{
             console.log("you are wrong son")
             console.log(wordArray)
         }
-
 });
 
 
